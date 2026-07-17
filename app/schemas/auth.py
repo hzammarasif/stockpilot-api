bb82c5bcda3e4a78f1296bbbd07533a3f9f3fefe
Muhammad Ammar Asif <hzammarasif@gmail.com>
2026-07-17 19:05:32 +0500

    jwt configuration edited

---
+++ app/schemas/auth.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from datetime import datetime
+from typing import Literal
+from uuid import UUID
+
+class UserRegister(BaseModel):
+    model_config = ConfigDict(
+        str_strip_whitespace=True,
+    )
+
+    username: str = Field(min_length=3, max_length=50)
+    email: EmailStr
+    password: str = Field(min_length=8, max_length=128)
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class TokenPayload(BaseModel):
+    sub: UUID
+    type: Literal["access", "refresh"]
+    exp: datetime
