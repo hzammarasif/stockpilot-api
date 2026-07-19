260eef31b7b1c3fff86d11bc3885b620d91f1014
Muhammad Ammar Asif <hzammarasif@gmail.com>
2026-07-19 15:47:51 +0500

    added a register router in (api/v1/auth.py)

---
+++ app/api/v1/auth.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from starlette import status
+
+from app.dependencies.auth import get_auth_service
+from app.schemas.auth import UserRegister
+from app.schemas.user import UserResponse
+from app.services.auth_service import AuthService
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"],
+)
+
+@router.post(
+    "/register",
+    response_model=UserResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+async def register(
+    data: UserRegister,
+    auth_service: AuthService = Depends(get_auth_service),
+) -> UserResponse:
+    return await auth_service.register_user(data)
--- app/main.py
+++ app/main.py
@@ -1,28 +1,22 @@
 from fastapi import FastAPI
+from app.api.router import api_router
 
 from app.core.config import settings
 from app.core.exception_handler import (
     register_exception_handlers,
 )
 
-
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
     debug=settings.DEBUG,
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
 
 register_exception_handlers(app)
 
-@app.get("/")
-async def root():
-    return {
-        "message": "Welcome to StockPilot API 🚀"
-    }
-
-
-@app.get("/health")
-async def health():
-    return {
-        "status": "healthy"
-    }
+app.include_router(
+    api_router,
+    prefix="/api/v1",
+)
